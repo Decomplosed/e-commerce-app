788799d37e98beebbe533735e96a5681e7a58982
Bart Frackiewicz <decomplosed@gmail.com>
2019-12-16 23:16:06 +0100

    adding filtering logic

--- repositories/users.js
+++ repositories/users.js
@@ -66,12 +66,32 @@ class UsersRepository {
 
     await this.writeAll(records)
   }
+
+  async getOneBy(filters) {
+    const records = await this.getAll()
+
+    for (let record of records) {
+      let found = true
+
+      for (let key in filters) {
+        if (record[key] !== filters[key]) {
+          found = false
+        }
+      }
+
+      if (found) {
+        return record
+      }
+    }
+  }
 }
 
 const test = async () => {
   const repo = new UsersRepository('users.json')
 
-  await repo.update('0e8sb0d5', { email: 'test@testing.com', password: 'password' })
+  const user = await repo.getOneBy({ email: 'test@testing.com', password: 'password' })
+
+  console.log(user)
 }
 
 test()
