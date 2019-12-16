23c6948ab2af8cd1e0db1155723e1fbdb2dcec97
Bart Frackiewicz <decomplosed@gmail.com>
2019-12-16 18:30:42 +0100

    added fs library and made a check if file exists

--- repositories/users.js
+++ repositories/users.js
@@ -1,9 +1,18 @@
+const fs = require('fs')
+
 class UsersRepository {
   constructor(filename) {
     if (!filename) {
       throw new Error('Creating a repository requires a filename')
     }
+
+    this.filename = filename
+    fs.accessSync(this.filename)
+  }
+
+  async checkForFile() {
+
   }
 }
 
-new UsersRepository()
+const repo = new UsersRepository()
