9cdbcebf912d5e34e94e52bf3d7f37b6a6d23170
Bart Frackiewicz <decomplosed@gmail.com>
2019-12-16 18:02:20 +0100

    added name atteribute to the inputs and POST method to the form

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ const app = express()
 app.get('/', (req, res) => {
   res.send(`
     <div>
-      <form>
-        <input placeholder="Email" />
-        <input placeholder="Password" />
-        <input placeholder="Password Confirmation" />
+      <form method="POST">
+        <input name="email" placeholder="Email" />
+        <input name="password" placeholder="Password" />
+        <input name="passwordConfirmation" placeholder="Password Confirmation" />
         <button>Sign Up</button>
       </form>
     </div>
