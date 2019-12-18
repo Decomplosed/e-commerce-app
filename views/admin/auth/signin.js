fb55b0d8e16da9d76083bd66dfad7ef58c1ca44d
Bart Frackiewicz <decomplosed@gmail.com>
2019-12-18 09:23:25 +0100

    changed the signin template to be a proper html

--- views/admin/auth/signin.js
+++ views/admin/auth/signin.js
@@ -1,11 +1,18 @@
 module.exports = () => {
   return `
-  <div>
-    <form method="POST">
-      <input name="email" placeholder="Email" />
-      <input name="password" placeholder="Password" />
-      <button>Sign In</button>
-    </form>
-  </div>
+  <!DOCTYPE html>
+  <html>
+    <head>
+    </head>
+    <body>
+      <div>
+        <form method="POST">
+          <input name="email" placeholder="Email" />
+          <input name="password" placeholder="Password" />
+          <button>Sign In</button>
+        </form>
+      </div>
+    </body>
+  </html>
   `
 }
