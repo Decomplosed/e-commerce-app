f69f8f146dc799a89ac4381331c64202564450b0
Bart Frackiewicz <decomplosed@gmail.com>
2019-12-18 09:23:39 +0100

    same for signup form

--- views/admin/auth/signup.js
+++ views/admin/auth/signup.js
@@ -1,13 +1,20 @@
 module.exports = ({ req }) => {
   return `
-  <div>
-    Your id is: ${req.session.userId}
-    <form method="POST">
-      <input name="email" placeholder="Email" />
-      <input name="password" placeholder="Password" />
-      <input name="passwordConfirmation" placeholder="Password Confirmation" />
-      <button>Sign Up</button>
-    </form>
-  </div>
+  <!DOCTYPE html>
+  <html>
+    <head>
+    </head>
+    <body>
+      <div>
+        Your id is: ${req.session.userId}
+        <form method="POST">
+          <input name="email" placeholder="Email" />
+          <input name="password" placeholder="Password" />
+          <input name="passwordConfirmation" placeholder="Password Confirmation" />
+          <button>Sign Up</button>
+        </form>
+      </div>
+    </body>
+  </html>
   `
 }
