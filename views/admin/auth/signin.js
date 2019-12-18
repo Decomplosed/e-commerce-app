a16ae35880182fb6a82bb2aebb3f1e0e7af5b601
Bart Frackiewicz <decomplosed@gmail.com>
2019-12-18 09:26:23 +0100

    reduced html template and created a layout file which will store all the templates

--- views/admin/auth/signin.js
+++ views/admin/auth/signin.js
@@ -1,18 +1,11 @@
 module.exports = () => {
   return `
-  <!DOCTYPE html>
-  <html>
-    <head>
-    </head>
-    <body>
-      <div>
-        <form method="POST">
-          <input name="email" placeholder="Email" />
-          <input name="password" placeholder="Password" />
-          <button>Sign In</button>
-        </form>
-      </div>
-    </body>
-  </html>
+    <div>
+      <form method="POST">
+        <input name="email" placeholder="Email" />
+        <input name="password" placeholder="Password" />
+        <button>Sign In</button>
+      </form>
+    </div>
   `
 }
--- views/admin/auth/signup.js
+++ views/admin/auth/signup.js
@@ -1,20 +1,13 @@
 module.exports = ({ req }) => {
   return `
-  <!DOCTYPE html>
-  <html>
-    <head>
-    </head>
-    <body>
-      <div>
-        Your id is: ${req.session.userId}
-        <form method="POST">
-          <input name="email" placeholder="Email" />
-          <input name="password" placeholder="Password" />
-          <input name="passwordConfirmation" placeholder="Password Confirmation" />
-          <button>Sign Up</button>
-        </form>
-      </div>
-    </body>
-  </html>
+    <div>
+      Your id is: ${req.session.userId}
+      <form method="POST">
+        <input name="email" placeholder="Email" />
+        <input name="password" placeholder="Password" />
+        <input name="passwordConfirmation" placeholder="Password Confirmation" />
+        <button>Sign Up</button>
+      </form>
+    </div>
   `
 }
