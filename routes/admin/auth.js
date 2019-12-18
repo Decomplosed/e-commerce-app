6745a18e81bd932572691b486ecf64e9393b82cf
Bart Frackiewicz <decomplosed@gmail.com>
2019-12-18 06:53:48 +0100

    moved html template of signup path to the new file

--- routes/admin/auth.js
+++ routes/admin/auth.js
@@ -5,15 +5,7 @@ const router = express.Router()
 
 router.get('/signup', (req, res) => {
   res.send(`
-    <div>
-      Your id is: ${req.session.userId}
-      <form method="POST">
-        <input name="email" placeholder="Email" />
-        <input name="password" placeholder="Password" />
-        <input name="passwordConfirmation" placeholder="Password Confirmation" />
-        <button>Sign Up</button>
-      </form>
-    </div>
+    
   `)
 })
 
