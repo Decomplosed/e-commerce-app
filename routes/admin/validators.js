40c2653f528c26f5c7c830aed2cd4967e656c644
Bart Frackiewicz <decomplosed@gmail.com>
2019-12-19 17:35:34 +0100

    created requireValidPassword method inside validators

--- routes/admin/validators.js
+++ routes/admin/validators.js
@@ -32,6 +32,7 @@ module.exports = {
           throw new Error('Passwords must match')
         }
       }),
+
   requireEmailExists:
     check('email')
       .trim()
@@ -43,6 +44,26 @@ module.exports = {
 
         if (!user) {
           throw new Error('Email not found!')
+        }
+      }),
+
+  requireValidPassword:
+    check('password')
+      .trim()
+      .custom(async (password, { req }) => {
+        const user = await usersRepo.getOneBy({ email: req.body.email })
+
+        if (!user) {
+          throw new Error('Invalid password')
+        }
+
+        const validPassword = await usersRepo.comparePasswords(
+          user.password,
+          password
+        )
+
+        if (!validPassword) {
+          throw new Error('Invalid password')
         }
       })
 }
