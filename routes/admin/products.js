521f92fea69c0393571963b4273cd9f1619b152d
Bart Frackiewicz <decomplosed@gmail.com>
2019-12-20 23:35:32 +0100

    require template

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const productsRepo = require('../../repositories/products')
+const productsNewTemplate = require('../../views/admin/products/new')
 
 const router = express.Router()
 
