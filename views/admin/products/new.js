d3bfbadee7c9f6ac41e9cb900ed1c480d93719d6
Bart Frackiewicz <decomplosed@gmail.com>
2019-12-20 23:34:19 +0100

    finished creating an html for the form

--- views/admin/products/new.js
+++ views/admin/products/new.js
@@ -4,7 +4,12 @@ const { getError } = require('../../helpers')
 module.exports = ({ errors }) => {
   return layout({
     content: `
-      
+      <form mthod=POST>
+        <input placeholder="Title" name="title" />
+        <input placeholder="Price" name="price" />
+        <input type="file" name="image" />
+        <button>Submit</button>
+      <form>
     `
   })
 }
