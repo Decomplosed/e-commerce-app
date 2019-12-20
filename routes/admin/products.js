ab485c2e2a70bd06d18bc03c9d7ddb8b71139030
Bart Frackiewicz <decomplosed@gmail.com>
2019-12-20 23:49:00 +0100

    require validationResult

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const { validationResult } = require('express-validator')
 const productsRepo = require('../../repositories/products')
 const productsNewTemplate = require('../../views/admin/products/new')
 const { requireTitle, requirePrice } = require('./validators')
