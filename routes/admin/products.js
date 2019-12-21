e1482cf1e624c1f35a1e4111e3a5558491e84e98
Bart Frackiewicz <decomplosed@gmail.com>
2019-12-21 22:19:18 +0100

    added missing / inside app.post - error resolved

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -6,27 +6,17 @@ const { requireTitle, requirePrice } = require('./validators')
 
 const router = express.Router()
 
-router.get('/admin/products', (req, res) => {
-
-})
+router.get('/admin/products', (req, res) => { })
 
 router.get('/admin/products/new', (req, res) => {
   res.send(productsNewTemplate({}))
 })
 
-router.post(
-  'admin/products/new',
-  [
-    requireTitle,
-    requirePrice
-  ],
-  (req, res) => {
-    const errors = validationResult(req)
-    console.log(errors)
+router.post('/admin/products/new', [requireTitle, requirePrice], (req, res) => {
+  const errors = validationResult(req)
+  console.log(errors)
 
-    console.log(req.body)
-
-    res.send('submitted')
-  })
+  res.send('submitted')
+})
 
 module.exports = router
