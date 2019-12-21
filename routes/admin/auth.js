cbdead68ef69207f75a801f066c57b23de83c418
Bart Frackiewicz <decomplosed@gmail.com>
2019-12-21 23:42:38 +0100

    require handleErrors inside auth.js

--- routes/admin/auth.js
+++ routes/admin/auth.js
@@ -1,6 +1,6 @@
 const express = require('express')
-const { check, validationResult } = require('express-validator')
 
+const { handleErrors } = require('./middlewares')
 const usersRepo = require('../../repositories/users')
 const signUpTemplate = require('../../views/admin/auth/signup')
 const signInTemplate = require('../../views/admin/auth/signin')
