a55333bcacc3ef7fbd99ec412f89b39ff082ae24
Bart Frackiewicz <decomplosed@gmail.com>
2019-12-22 23:16:39 +0100

    added missing async to the function

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -10,7 +10,7 @@ const { requireTitle, requirePrice } = require('./validators')
 const router = express.Router()
 const upload = multer({ storage: multer.memoryStorage() })
 
-router.get('/admin/products', (req, res) => {
+router.get('/admin/products', async (req, res) => {
   const products = await productsRepo.getAll()
   res.send(productsIndexTemplate({ products }))
 })
