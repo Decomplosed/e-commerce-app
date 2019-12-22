e8dbace6fe7156edcff98e23246b253404c3eea0
Bart Frackiewicz <decomplosed@gmail.com>
2019-12-22 23:54:54 +0100

    added missing call of the function

--- repositories/repository.js
+++ repositories/repository.js
@@ -16,7 +16,7 @@ module.exports = class Repository {
   }
 
   async create(attrs) {
-    attrs.id = this.randomId
+    attrs.id = this.randomId()
 
     const records = await this.getAll()
     records.push(attrs)
