56f64eaf7f18ffcd644d91f0afa53ef87adafdbb
Bart Frackiewicz <decomplosed@gmail.com>
2019-12-26 12:02:19 +0100

    changed cart to be let overwritten by logic inside validation

--- routes/carts.js
+++ routes/carts.js
@@ -4,11 +4,12 @@ const cartsRepo = require('../repositories/carts')
 const router = express.Router()
 
 router.post('/cart/products', async (req, res) => {
+  let cart
   if (!req.session.cartId) {
-    const cart = await cartsRepo.create({ items: [] })
+    cart = await cartsRepo.create({ items: [] })
     req.session.cartId = cart.id
   } else {
-    const cart = await cartsRepo.getOne(req.session.cartId)
+    cart = await cartsRepo.getOne(req.session.cartId)
   }
 
   res.send('Product added to cart')
