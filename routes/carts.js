a0ec66bc1b61750a20704fe75944d8c739b45415
Bart Frackiewicz <decomplosed@gmail.com>
2019-12-26 12:01:32 +0100

    if cart exists - assign its value to the session cartID

--- routes/carts.js
+++ routes/carts.js
@@ -6,10 +6,9 @@ const router = express.Router()
 router.post('/cart/products', async (req, res) => {
   if (!req.session.cartId) {
     const cart = await cartsRepo.create({ items: [] })
-
     req.session.cartId = cart.id
   } else {
-
+    const cart = await cartsRepo.getOne(req.session.cartId)
   }
 
   res.send('Product added to cart')
