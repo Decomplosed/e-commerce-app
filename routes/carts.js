3c3b1c101ac33818296bd915e027f5bea441c72f
Bart Frackiewicz <decomplosed@gmail.com>
2019-12-26 11:48:43 +0100

    proof of concept, the id is properly get nbyy request

--- routes/carts.js
+++ routes/carts.js
@@ -2,4 +2,10 @@ const express = require('express')
 
 const router = express.Router()
 
+router.post('/cart/products', (req, res) => {
+  
+
+  res.send('Product added to cart')
+})
+
 module.exports = router
