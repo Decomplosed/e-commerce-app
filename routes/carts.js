4a8815b5dce931bdaa993ce5de05ccc032e143f8
Bart Frackiewicz <decomplosed@gmail.com>
2019-12-26 12:37:32 +0100

    added the functionality to remove items from the cart

--- routes/carts.js
+++ routes/carts.js
@@ -45,4 +45,16 @@ router.get('/cart', async (req, res) => {
   res.send(cartShowTemplate({ items: cart.items }))
 })
 
+router.post('/cart/products/delete', async (req, res) => {
+  const { itemId } = req.body
+
+  cart = await cartsRepo.getOne(req.session.cartId)
+
+  const items = cart.items.filter(item => item.id !== itemId)
+
+  await cartsRepo.update(req.session.cartId, { items })
+
+  res.redirect('/cart')
+})
+
 module.exports = router
