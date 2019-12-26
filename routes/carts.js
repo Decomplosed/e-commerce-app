07228e48a2dd16e9be05a5505ae104583830f834
Bart Frackiewicz <decomplosed@gmail.com>
2019-12-26 11:56:06 +0100

    started creating a logic for cardId sessio

--- routes/carts.js
+++ routes/carts.js
@@ -3,7 +3,11 @@ const express = require('express')
 const router = express.Router()
 
 router.post('/cart/products', (req, res) => {
-  
+  if (!req.session.cartId) {
+
+  } else {
+
+  }
 
   res.send('Product added to cart')
 })
